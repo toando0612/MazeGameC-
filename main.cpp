c53c155c5d50539afedf9358ff36a14f4b7fe8e0
Toan Do <toando0612@Toans-MacBook-Pro.local>
2019-04-03 20:28:20 +0700

    first commit

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <type_traits>
 using namespace std;
 
 typedef array<int, 4> edge;
+
+// Returns true if x is in range [low..high], else false
+bool inRange(unsigned low, unsigned high, unsigned x)
+{
+    return  ((x-low) <= (high-low));
+}
 
 
 class Cell {
@@ -15,9 +22,15 @@ class Cell {
         array<int, 2> coord;
     public://method or action on the attribubtes
         void setedge_list(array<edge, 4> new_edge_list){edge_lists = new_edge_list;}
-        auto getedge_list(){return edge_lists;}
         void setvisited(bool status){visited = status;}
         void setcoord(array<int, 2> newCoord){coord = newCoord;}
+
+        auto getedge_list(){return edge_lists;}
+        bool getvisited(){return visited;}
+
+
+
+
 
 
 
@@ -26,11 +39,14 @@ class MazeGame {
     private:
         vector<vector<Cell>> maze;
         array<int, 2> currentLocation;
+        vector<int> neighbours;
         int height;
         int width;
     public:
     //methods actions on the attributes
         void creatingMaze(){
+        //1st draw of maze
+            currentLocation = {1,1};
             vector<vector<Cell>> temp_maze;
             for (auto i = 0; i < width; i++)
             {
@@ -51,6 +67,32 @@ class MazeGame {
                 temp_maze.push_back(colums);
             }
             setmaze(temp_maze);
+            int x = currentLocation[0];
+            int y = currentLocation[1];
+            cout << "x,y = " << x <<",";
+            cout << y << endl;
+            //become of maze
+//            if (x == 0 ){
+//                if (y == 0){
+//                    //top left corner
+//                } else if(y == height-1){
+//                    //bottom left corner
+//                } else {
+//                    // left edge
+//                }
+//            } else if (y == 0)
+            if (inRange(0, width-1, x+1) && inRange(0, height -1, y) && maze[x+1][y].getvisited() == false){ //check east neighbor
+                neighbours.push_back(0);
+            }
+            if (inRange(0, width-1, x) && inRange(0, height -1, y + 1) && maze[x][y+1].getvisited()== false){//check south neighbor
+                neighbours.push_back(1);
+            }
+            if (inRange(0, width-1, x-1) && inRange(0, height -1, y) && maze[x-1][y].getvisited()== false){//check west neighbor
+                neighbours.push_back(2);
+            }
+            if (inRange(0, width-1, x) && inRange(0, height -1, y - 1) && maze[x][y-1].getvisited()== false){//check north  neighbor
+                neighbours.push_back(3);
+            }
 
         }
         void toCoord(){
@@ -77,8 +119,11 @@ class MazeGame {
                         cout << endl;
                     }
                 }
-
             }
+            for(int k = 0; k < neighbours.size() ; ++k) {
+                cout << neighbours[k];
+            }
+            neighbours = {};
 
         }
         void constructor(int h, int w ){setheight(h);setwidth(w);}
@@ -88,7 +133,6 @@ class MazeGame {
         void setwidth(int w){width = w;}
         int getheight(){return height;}
         int getwidth(){return width;}
-
 
 };
 
